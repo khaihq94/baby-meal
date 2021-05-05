925d77eba341b204a61aed89522c1ceae86da607
hqkhai <khai.ho@axonactive.com>
2021-05-05 11:21:58 +0700

    Initial commit

---
+++ src/main/java/vn/hqkhai/babymeal/BabymealServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.hqkhai.babymeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabymealServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabymealServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/hqkhai/babymeal/BabymealServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.hqkhai.babymeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabymealServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
